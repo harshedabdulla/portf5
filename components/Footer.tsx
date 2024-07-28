097468719c54a20d9a00a1ff8e7bcd911930e3cf
harshedabdulla <harshed2004@gmail.com>
2024-07-28 20:03:18 +0530

    basic complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,10 @@
 import { FaLocationArrow } from "react-icons/fa6";
 import { socialMedia } from "@/data";
 import MagicButton from "./ui/MagicButton";
-import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="w-full pt-8 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
